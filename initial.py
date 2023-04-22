56ebf7e2f91d0cc0c519e184fa73e139bc9e28c4
Ian Su <ensu.tw@gmail.com>
2023-04-23 01:02:38 +0800

    parse and add investor.csv file

--- initial.py
+++ initial.py
@@ -1,5 +1,8 @@
 from lxml import etree
 import pandas as pd
+from typing import List
+from urllib.parse import urlparse
+from datetime import datetime
 
 
 def _my_cleanup(text):
@@ -112,10 +115,22 @@ def parse_investor_page(html):
     if len(company_name) == 0:
         company_name = root.xpath(".//*[*[text()='企業名']]/dd/text()")
 
-    if len(company_name) == 0 or len(urls) == 0:
-        return {}
+    address = root.xpath(".//*[*[text()='Address']]/dd/text()")
+    establish_date = root.xpath(".//*[*[text()='Founded Date']]/dd/text()")
+    kinds = root.xpath(".//*[*[text()='Type']]/dd/text()")
+    attributes = root.xpath(".//*[*[text()='Industry']]/dd/text()")
+    alias = root.xpath(".//*[*[text()='old or alias']]/dd/text()")
+    snss = root.xpath(".//*[*[text()='SNS']]/dd//@href")
 
-    return {"Company name": company_name[0], "website": urls[0] if len(urls) > 0 else ""}
+    return {"Company name": company_name[0] if len(company_name) > 0 else "",
+            "website": urls[0] if len(urls) > 0 else "",
+            "address": address[0] if len(address) > 0 else "",
+            "founded date": establish_date[0] if len(establish_date) > 0 else "",
+            "kinds": kinds[0] if len(kinds) > 0 else "",
+            "attributes": attributes[0] if len(attributes) > 0 else "",
+            "alias": alias[0] if len(alias) > 0 else "",
+            "snss": snss
+            }
 
 
 def post_process_investor_df(df):
@@ -237,3 +252,32 @@ def map_acq_amount_to_num(acq_mount_str: str) -> int:
         return int(acq_mount_str.replace(",", "").replace("yen", ""))
     else:
         raise Exception("Unseen acq amount: {}".format(acq_mount_str))
+
+
+CB_INVESTOR_TYPES_MAPPING = {
+    "business corporation": "corporate_venture_capital",
+    "": "",
+    "VCs": "venture_capital",
+    "Financial institution": "private_equity_firm",
+    "Overseas VCs": "venture_capital",
+    "Overseas business corporation": "corporate_venture_capital",
+    "Local governments/universities/foundations/other funds": "government_office",
+    "Overseas financial institution": "private_equity_firm",
+    "Others": "",
+    "individual/private company": "",
+    "Crowdfunding": "",
+}
+
+def get_social_url(urls: List[str], social_keyword: str) -> str:
+    if isinstance(urls, list):
+        for i in urls:
+            host = urlparse(i).netloc
+            if social_keyword in host:
+                return i
+    return "" 
+
+def try_handle_date(date_str: str) -> str:
+    try:
+        return datetime.strptime(date_str, "%B %Y").strftime("%Y-%m-%d")
+    except:
+        return ''
